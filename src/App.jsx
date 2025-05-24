503fa2a49ddda2c9cd2f2bc3961829640e751012
AyushDeuja <ayushdeuja11@gmail.com>
2025-05-24 09:01:18 +0545

    removed comments and unwanted codes

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,16 @@
 import "./App.css";
 import NavBar from "./NavBar/NavBar";
 import Footer from "./Footer/Footer";
-import ToggleTheme from "./ToggleTheme/ToggleTheme";
 import MovieCard from "./MovieCard/MovieCard";
-import GetAllMovies from "./backendIntegration/GetAllMovies";
 
 function App() {
   return (
     <>
       <NavBar />
       <MovieCard />
-      {/* <ToggleTheme /> */}
       <div style={{ marginTop: "150px" }}>
         <Footer />
       </div>
-      {/* <GetAllMovies /> */}
     </>
   );
 }
